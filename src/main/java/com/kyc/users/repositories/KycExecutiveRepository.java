f7048b993b3d93e534f6412f301a865641dcaba6
LHPH <luislyly@hotmail.com>
2025-01-17 19:08:35 -0600

    Fix error in KycExecutiveRepository

--- src/main/java/com/kyc/users/repositories/KycExecutiveRepository.java
+++ src/main/java/com/kyc/users/repositories/KycExecutiveRepository.java
@@ -3,7 +3,7 @@ package com.kyc.users.repositories;
 import com.kyc.users.entity.KycExecutive;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface KycExecutiveRepository extends JpaRepository<Long, KycExecutive> {
+public interface KycExecutiveRepository extends JpaRepository<KycExecutive,Long> {
 
     KycExecutive findByIdUser(Long idUser);
 }
